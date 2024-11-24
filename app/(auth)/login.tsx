edfbc51626cc08389a9030e09f3288698fa01e07
arthurR3 <hedzjose014@gmail.com>
2024-11-24 16:01:16 -0600

    updated login

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, SafeAreaView, Alert, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Alert, ActivityIndicator, Modal } from 'react-native';
 import { Link, router } from 'expo-router';
 import React, { useState } from 'react';
 
@@ -8,7 +8,7 @@ import { useUsuarioContext } from '@/components/context/userContext';
 import useValidation from '@/hooks/validation/useValidation';
 import AuthService from '@/services/auth.service';
 import { jwtDecode } from 'jwt-decode';
-import { UserData } from '@/interfaces/auth.interface';
+import { DecodeToken, UserData } from '@/interfaces/auth.interface';
 import Encuesta from '@/components/Encuesta';
 
 
@@ -18,7 +18,7 @@ export default function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [secureText, setSecureText] = useState(true);
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
   const { emailError, passwordError, validationEmail, validationPassword } = useValidation();
   const [showSurveyModal, setShowSurveyModal] = useState(false);
   const [idUser, setIdUser] = useState<number | null>(null);
@@ -26,7 +26,7 @@ export default function LoginScreen() {
 
   const handleSurveyComplete = () => {
     setShowSurveyModal(false);
-    Alert.alert('Encuesta completa','Gracias por su participación!')
+    Alert.alert('Encuesta completa', 'Gracias por su participación!')
     setTimeout(() => router.replace('/'), 1000);
 
   };
@@ -41,24 +41,27 @@ export default function LoginScreen() {
       const response = await AuthService.login(email, password);
 
       if (response.success) {
-        // Iniciar sesión y redirigir si tiene éxito
         dispatch({ type: 'login', payload: response.data.data });
-        const token = jwtDecode<UserData>(response.data.data)
-        if (token.showSurvey) {
+        const token = jwtDecode<DecodeToken>(response.data.data);
+        const userToken: UserData = token.user
+        // Verifica si se debe mostrar la encuesta y asigna el ID de usuario
+        if (userToken.showSurvey) {
           setShowSurveyModal(true);
-          setIdUser(token.idUser);
-        }
-        if(!token.showSurvey){
+          setIdUser(userToken.idUser);
+          setLoading(false);
+        } else {
           router.replace('/');
+          setLoading(false);
         }
-      } else {
-        // Mostrar la alerta basada en el código de error
-        Alert.alert('Error Inesperado', response.message);
-      }
 
-      setLoading(false);
+        // Detiene el indicador de carga después de la gestión del estado
+      } else {
+        Alert.alert('Error Inesperado', response.message);
+        setLoading(false); // Asegura desactivar `loading` también en caso de error
+      }
     }
   };
+
 
   const toogleAccount = () => {
     router.push('/(auth)/account');
@@ -67,48 +70,56 @@ export default function LoginScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <>
-      <View>
-        <Text style={styles.welcome}>Bienvenido</Text>
-        <Text style={styles.subtitle}>Inicia sesión para continuar.</Text>
-        <View style={{ marginTop: 40 }}>
-          <Text style={styles.inputText}>CORREO ELECTRONICO</Text>
-          <InputLogin
-            placeholder='example@example.com'
-            image='mail'
-            keyboardType='email-address'
-            value={email}
-            onChangeText={setEmail}
-            bolError={!!emailError}
-            strError={emailError}
-          />
-          <Text style={styles.inputText}>CONTRASEÑA</Text>
-          <InputLogin
-            placeholder='********'
-            image='lock-closed'
-            secureText={secureText}
-            bolGone={true}
-            onPressIcon={() => setSecureText(!secureText)}
-            value={password}
-            onChangeText={setPassword}
-            bolError={!!passwordError}
-            strError={passwordError}
-          />
+        <View>
+          <Text style={styles.welcome}>Bienvenido</Text>
+          <Text style={styles.subtitle}>Inicia sesión para continuar.</Text>
+          <View style={{ marginTop: 40 }}>
+            <Text style={styles.inputText}>CORREO ELECTRONICO</Text>
+            <InputLogin
+              placeholder='example@example.com'
+              image='mail'
+              keyboardType='email-address'
+              value={email}
+              onChangeText={setEmail}
+              bolError={!!emailError}
+              strError={emailError}
+            />
+            <Text style={styles.inputText}>CONTRASEÑA</Text>
+            <InputLogin
+              placeholder='********'
+              image='lock-closed'
+              secureText={secureText}
+              bolGone={true}
+              onPressIcon={() => setSecureText(!secureText)}
+              value={password}
+              onChangeText={setPassword}
+              bolError={!!passwordError}
+              strError={passwordError}
+            />
+          </View>
+
+          {/* Mostrar el indicador de carga si loading es verdadero */}
+          {loading ? (
+            <ActivityIndicator color="#452e3f" style={styles.loading} />
+          ) : (
+            <CustomButton title='Iniciar Sesión' onPress={handleSubmit} disabled={loading} />
+          )}
+
+          <Link style={{ margin: 15, fontSize: 20, alignSelf: 'center' }} href={'/(auth)/forgetted'}><Text>Recuperar contraseña</Text></Link>
+          <CustomButton title='Crear cuenta' onPress={toogleAccount} disabled={false} />
         </View>
-
-        {/* Mostrar el indicador de carga si loading es verdadero */}
-        {loading ? (
-          <ActivityIndicator  color="#452e3f" style={styles.loading} />
-        ) : (
-          <CustomButton title='Iniciar Sesión' onPress={handleSubmit} disabled={loading} />
-        )}
-
-        <Link style={{ margin: 15, fontSize: 20, alignSelf: 'center' }} href={'/(auth)/forgetted'}><Text>Recuperar contraseña</Text></Link>
-        <CustomButton title='Crear cuenta' onPress={toogleAccount}  disabled={false} />
-      </View>
       </>
       {showSurveyModal && idUser !== null && (
-        <Encuesta id_user={idUser} onComplete={handleSurveyComplete}/>
-      )}
+        <Modal
+        visible={showSurveyModal}
+        transparent
+        animationType="fade"
+        onRequestClose={() => { }}
+      >
+        <View style={styles.modalContainer}>
+          <Encuesta id_user={idUser} onComplete={handleSurveyComplete} />
+        </View>
+      </Modal>)}
     </SafeAreaView>
   );
 }
@@ -138,5 +149,11 @@ const styles = StyleSheet.create({
   },
   loading: {
     marginVertical: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo semitransparente
   },
 });
